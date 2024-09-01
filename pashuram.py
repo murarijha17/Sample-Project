fa470aaa47600fe850ae5f6aeb9e1e3a8bcecf19
Murari Jha <intern9870@gmail.com>
2024-09-01 21:44:14 +0530

    all

---
+++ pashuram.py
@@ -0,0 +1,12 @@
+import os
+List1=[1,2,3,4,5]
+List1.reverse()
+print(List1)
+List2=[1,2,3,4,5]
+List2.sort()
+print(List1)
+List3=[]
+List3=List1.copy()
+print(List3)
+indexvalue=List2[1:3]
+print(indexvalue)
